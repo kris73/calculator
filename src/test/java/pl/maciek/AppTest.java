98238d21298391e43895178a36bbaf64ed096074
krystianP <krystian.przysiezniak@teaminternational.com>
2020-10-05 18:38:48 +0200

    initial commit

---
+++ src/test/java/pl/maciek/AppTest.java
@@ -0,0 +1,20 @@
+package pl.maciek;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
